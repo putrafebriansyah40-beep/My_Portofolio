97e7934b9018f55e54165e8c003ff0e4b1eaeac4
unknown <putrafebriansyah40@gmail.com>
2026-08-24 17:20:56 +0700

    fix: configure vercel serverless storage path and enable debug mode

--- api/index.php
+++ api/index.php
@@ -5,4 +5,7 @@
  * Forwards requests to Laravel's public/index.php
  */
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp');
+$app->handleRequest(Illuminate\Http\Request::capture());

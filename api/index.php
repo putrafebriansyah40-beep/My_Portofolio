a2b37d16e0694f392d64ca89e32b58a4efcb379e
unknown <putrafebriansyah40@gmail.com>
2026-08-24 17:24:07 +0700

    fix: add diagnostic try-catch to api/index.php

--- api/index.php
+++ api/index.php
@@ -8,4 +8,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 $app = require __DIR__ . '/../bootstrap/app.php';
 $app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp');
-$app->handleRequest(Illuminate\Http\Request::capture());
+try {
+    $app->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    echo "<pre>";
+    echo $e->getMessage() . "\n\n";
+    foreach ($e->getTrace() as $trace) {
+        $class = $trace['class'] ?? '';
+        $type = $trace['type'] ?? '';
+        $function = $trace['function'] ?? '';
+        $file = $trace['file'] ?? 'unknown file';
+        $line = $trace['line'] ?? 'unknown line';
+        echo "{$class}{$type}{$function} in {$file}:{$line}\n";
+    }
+    echo "</pre>";
+}
